83ca0aabf73e8ad928dff4bd80831ab360abbc8e
James (DPC) <69664712+JamesDPC@users.noreply.github.com>
2024-12-10 18:06:25 +1100

    Improve retrieval of SiteTree record data for the TypesenseSearchResult

--- src/Extensions/SiteTreeSearchResult.php
+++ src/Extensions/SiteTreeSearchResult.php
@@ -2,9 +2,13 @@
 
 namespace NSWDPC\Typesense\CMS\Extensions;
 
+use DNADesign\Elemental\Extensions\ElementalPageExtension;
 use NSWDPC\Search\Typesense\Traits\TypesenseDefaultFields;
 use NSWDPC\Search\Typesense\Models\TypesenseSearchResult;
 use SilverStripe\Core\Extension;
+use SilverStripe\ORM\FieldType\DBField;
+use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\ORM\FieldType\DBText;
 
 /**
  * Provides Typesense search result for SiteTree
@@ -18,20 +22,48 @@ class SiteTreeSearchResult extends Extension {
      */
     public function getTypesenseSearchResult(): TypesenseSearchResult {
         $owner = $this->getOwner();
-        $abstract = trim($owner->dbObject('Content')->FirstParagraph() ?? '');
-        // TODO: elemental support.. if a Page has that
+        $supportsElemental = class_exists(ElementalPageExtension::class) && $owner->supportsElemental();
 
-        return TypesenseSearchResult::create([
+        // try to determine the search abstract
+        if($owner->hasMethod('getSearchResultAbstract')) {
+            $abstract = (string)$owner->getSearchResultAbstract();
+        } else if($owner->hasField('Abstract')) {
+            // maybe the model provides a search abstract
+            $abstract = $owner->dbObject('Abstract');
+        } else if($supportsElemental) {
+            // if elemental is supported
+            $abstract = DBField::create_field(
+                DBHTMLText::class,
+                $owner->getElementsForSearch()
+            )->setProcessShortcodes(false)
+            ->FirstSentence();
+        } else {
+            // use the first sentence of the content
+            $content = $owner->dbObject('Content');
+            if($content instanceof DBHTMLText) {
+                $content = $content->setProcessShortcodes(false);
+            }
+            $content = $content->FirstSentence();
+        }
+
+        $data = [
             'Title' => $owner->Title ?? '',
-            'Date' => null,
+            'Date' => $owner->dbObject('LastEdited')->Format('d MMMM y'),
             'Link' => $owner->Link() ?? '',
             'ImageURL' => '',
             'ImageAlt' => '',
             'Label' => '',
             'Labels' => [],
-            'Abstract' => $abstract,
+            'Abstract' => strip_tags(trim($abstract)),
             'Info' => ''
-        ]);
+        ];
+
+        if($owner->hasMethod('decorateTypesenseSearchResult')) {
+            // Allow project-level decoration of the search result
+            $owner->decorateTypesenseSearchResult($data);
+        }
+
+        return TypesenseSearchResult::create($data);
     }
 
 }
