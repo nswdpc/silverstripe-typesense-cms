48ac2c086e951f2d9c70c616f1aa13b4f760590d
JamesDPC <69664712+JamesDPC@users.noreply.github.com>
2025-10-03 05:59:44 +0000

    [rector] Automated updates generated by rector configuration

--- src/Controllers/TypesenseSearchPageController.php
+++ src/Controllers/TypesenseSearchPageController.php
@@ -94,6 +94,7 @@ class TypesenseSearchPageController extends \PageController
                     // no scope
                     $searchScope = [];
                 }
+
                 $searchKey = $model->SearchKey ?? '';
                 $paginatedList = $handler->doSearch($collection, $term, $pageStart, $perPage, $searchScope, $searchKey);
             } catch (\JsonException $jsonException) {
