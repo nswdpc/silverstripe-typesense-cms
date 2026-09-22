45a514af053be5a94c8da2c417193bc3bf5f8e5e
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2026-09-22 14:54:36 +1000

    (dev) add ignore annotation for phpstan

--- tests/php/TestOnly/PageController.php
+++ tests/php/TestOnly/PageController.php
@@ -8,7 +8,7 @@ use SilverStripe\CMS\Controllers\ContentController;
  * See Page.php in this directory for why this fixture exists, and why it is not `implements TestOnly`
  * (TypesenseSearchPageController extends this class).
  * @template T of \Page
- * @extends \SilverStripe\CMS\Controllers\ContentController<T>
+ * @extends \SilverStripe\CMS\Controllers\ContentController<T> @phpstan-ignore generics.notSupportedBound
  */
 class PageController extends ContentController
 {
