22113f257a00ad2d775b0fc4bafbbea0c4c1e6af
James (PD) <69664712+JamesDPC@users.noreply.github.com>
2026-09-24 14:07:14 +1000

    (dev) remove unnecessary stub files as the cms phpunit bootstrap creates these, remove from autoload configuration in composer.json

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -1,10 +1,2 @@
 <?php
-
-declare(strict_types=1);
-
 require __DIR__ . '/../../vendor/silverstripe/cms/tests/bootstrap.php';
-
-print "I am requiring..\n";
-
-require __DIR__ . '/Stubs/Page.php';
-require __DIR__ . '/Stubs/PageController.php';
